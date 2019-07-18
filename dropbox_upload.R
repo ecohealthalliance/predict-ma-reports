176e5532345f02f24f79fdb5d2a00b36aab10607
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-07-18 15:08:23 -0400

    Add back outputs folder.  Template dropbox_upload script.

---
+++ dropbox_upload.R
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+
+library(rdrop2)
+library(cyphr)
+library(sodium)
+library(base64enc)
+#
+#
+# token <- decrypt(readRDS("token.rds.encrypted"), key = key_sodium(base64decode(Sys.getenv("SODIUM_KEY"))))
+# to_upload <- list.files("outputs", pattern = "(html|xlsx)$", full.names = TRUE)
+# for (f in to_upload) {
+#   drop_upload(f, path  = "shared/eidith-cleaning-reports-outputs/", dtoken = token, autorename = FALSE)
+# }
