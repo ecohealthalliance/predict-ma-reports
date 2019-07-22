d0aeb3ebdae6101a1de06648e54e46dcd63b1a5d
Noam Ross <ross@ecohealthalliance.org>
2019-07-22 13:30:00 -0400

    Build countries but only error after

--- make.R
+++ make.R
@@ -1,23 +1,29 @@
 #!/usr/bin/env Rscript
 h <- here::here
+library(purrr)
+library(future)
 
 countries <- c(
-  # "Bangladesh",
-  # "China",
-  # "Egypt",
-  # "India",
-  # "Indonesia",
-  # "Ivory Coast",
-  # "Jordan",
-  # "Liberia",
-  # "Republic of Congo",
- # "South Sudan",
+  "Bangladesh",
+  "China",
+  "Egypt",
+  "India",
+  "Indonesia",
+  "Ivory Coast",
+  "Jordan",
+  "Liberia",
+  "Republic of Congo",
+  "South Sudan",
   "Thailand"
 )
 
-for (country in countries) {
-  rmarkdown::render("report-template.Rmd",
-                    output_file = paste0(country, "-ma-report.html"),
-                    output_dir = h("outputs"),
-                    params = list(country = country))
-}
+reports <- map(countries, funcction(country) {
+  safely(rmarkdown::render)("report-template.Rmd",
+                            output_file = paste0(country, "-ma-report.html"),
+                            output_dir = h("outputs"),
+                            params = list(country = country))
+})
+
+errors <- map_lgl(reports, ~!is.null(.$error))
+
+if (any(errors)) stop(paste("Error building", paste(counties[errors], collapse = ", ")))
