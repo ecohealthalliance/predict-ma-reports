ee38825722c77a8ab67e42ce87487ece6be0be26
Noam Ross <ross@ecohealthalliance.org>
2019-07-26 14:49:52 +0700

    Collect errors still using for() loop for countries

--- make.R
+++ make.R
@@ -3,13 +3,16 @@ library(purrr)
 h <- here::here
 
 countries <- eidith::eha_countries()[-c(10, 11)]
+reports <- list()
 
-reports <- for(country in countries) {
+for(country in countries) {
 
-  safely(rmarkdown::render, quiet = FALSE)("report-template.Rmd",
-                            output_file = paste0(country, "-ma-report.html"),
-                            output_dir = h("outputs"),
-                            params = list(country = country))
+  reports[[country]] <- safely(rmarkdown::render, quiet = FALSE)(
+    "report-template.Rmd",
+    output_file = paste0(country, "-ma-report.html"),
+    output_dir = h("outputs"),
+    params = list(country = country)
+  )
 }
 
 errors <- map_lgl(reports, ~!is.null(.$error))
