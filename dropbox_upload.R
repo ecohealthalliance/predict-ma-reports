f9fc25774b26409f458f50083b785ef9391578c8
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-08-08 12:59:17 -0400

    Dropbox upload

--- dropbox_upload.R
+++ dropbox_upload.R
@@ -1,13 +1,24 @@
 #!/usr/bin/env Rscript
 
-# library(rdrop2)
-# library(cyphr)
-# library(sodium)
-# library(base64enc)
+library(rdrop2)
+library(cyphr)
+library(sodium)
+library(base64enc)
+
+# Run once:
+# # Generate dropbox token
+# db_token <- rdrop2::drop_auth()
+# saveRDS(db_token, file = "token.rds")
 #
+# # Generate key
+# key <-sodium::keygen()
+# key64 <- base64enc::base64encode(key)
 #
-# token <- decrypt(readRDS("token.rds.encrypted"), key = key_sodium(base64decode(Sys.getenv("SODIUM_KEY"))))
-# to_upload <- list.files("outputs", pattern = "(html|xlsx)$", full.names = TRUE)
-# for (f in to_upload) {
-#   drop_upload(f, path  = "shared/eidith-cleaning-reports-outputs/", dtoken = token, autorename = FALSE)
-# }
+# # Encrypt token
+# cyphr::encrypt_file("token.rds", cyphr::key_sodium(base64decode(key64)), "token.rds.encrypted")
+
+token <- decrypt(readRDS("token.rds.encrypted"), key = key_sodium(base64decode(Sys.getenv("SODIUM_KEY"))))
+to_upload <- list.files("outputs", pattern = ".html", full.names = TRUE)
+for (f in to_upload) {
+  drop_upload(f, path  = "predict-ma-reports", dtoken = token, autorename = FALSE)
+}
