135f5b2f3ee95070a3da45a6ff2307b179de9793
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-11-18 14:30:59 -0500

    fix field names

--- scripts/generate_viral_species_lookups.R
+++ scripts/generate_viral_species_lookups.R
@@ -25,12 +25,12 @@ a2 <- a2 %>%
       family == "Sciuridae" ~ "rodents/shrews",
       TRUE ~ taxa_group
     ),
-    species_scientific_name = case_when(
-      species_scientific_name == "Mus musculus cf. castaneus" ~ "Mus musculus",
-      species_scientific_name == "Syncerus Caffer" ~ "Syncerus caffer",
-      str_detect(species_scientific_name, " cf. ") ~ str_replace(species_scientific_name, " cf. ", " "),
-      str_count(species_scientific_name, " ") == 2 ~ word(species_scientific_name, 1, 2),
-      TRUE ~ species_scientific_name
+    scientific_name = case_when(
+      scientific_name == "Mus musculus cf. castaneus" ~ "Mus musculus",
+      scientific_name == "Syncerus Caffer" ~ "Syncerus caffer",
+      str_detect(scientific_name, " cf. ") ~ str_replace(scientific_name, " cf. ", " "),
+      str_count(scientific_name, " ") == 2 ~ word(scientific_name, 1, 2),
+      TRUE ~ scientific_name
     )
   )
 
@@ -68,7 +68,7 @@ t2 <- t2 %>%
 
 d2 <- left_join(e2, a2, by = c("event_name", "project")) %>%
   left_join(s2, by = c("animal_id" = "animal_human_id", "project")) %>%
-  left_join(t2, by = c("specimen_id", "gains4_specimen_id", "project"))
+  left_join(t2, by = c("specimen_id", "animal_id", "gains4_specimen_id", "project"))
 
 d2 %>%
   filter(
@@ -90,14 +90,14 @@ viral_detections_by_species_and_site <- d2 %>%
     !is.na(test_requested),
     !is.na(viral_species)
   ) %>%
-  group_by(country, viral_species, species_scientific_name, animal_id,
+  group_by(country, viral_species, scientific_name, animal_id,
            site_latitude, site_longitude) %>%
   summarize(virus_detected = ifelse(sum(virus_detected) > 0, 1, 0)) %>%
-  group_by(country, viral_species, species_scientific_name,
+  group_by(country, viral_species, scientific_name,
            site_latitude, site_longitude) %>%
   summarize(n_animals_w_detections = sum(virus_detected)) %>%
   ungroup() %>%
-  arrange(country, viral_species, species_scientific_name, site_latitude, site_longitude)
+  arrange(country, viral_species, scientific_name, site_latitude, site_longitude)
 
 # Generate a table showing the viral test types that could result in detection of a given
 # virus
@@ -112,20 +112,20 @@ virus_test_pairs <- d2 %>%
 # detected for each species at each site
 
 viral_testing_by_species_and_site <- d2 %>%
-  distinct(country, species_scientific_name, animal_id,
+  distinct(country, scientific_name, animal_id,
            site_latitude, site_longitude, test_requested) %>%
   left_join(virus_test_pairs, ., by = "test_requested") %>%
   # roll up viral testing summary to animal level
-  distinct(viral_species, country, species_scientific_name, animal_id,
+  distinct(viral_species, country, scientific_name, animal_id,
            site_latitude, site_longitude) %>%
   # roll up viral testing summary to virus by country by species by site level
-  group_by(viral_species, country, species_scientific_name, site_latitude, site_longitude) %>%
+  group_by(viral_species, country, scientific_name, site_latitude, site_longitude) %>%
   summarize(n_animals_tested = n()) %>%
   ungroup() %>%
-  arrange(country, viral_species, species_scientific_name, site_latitude, site_longitude)
+  arrange(country, viral_species, scientific_name, site_latitude, site_longitude)
 
 left_join(viral_testing_by_species_and_site, viral_detections_by_species_and_site,
-          by = c("viral_species", "country", "species_scientific_name",
+          by = c("viral_species", "country", "scientific_name",
                  "site_latitude", "site_longitude")) %>%
   mutate(n_animals_w_detections = ifelse(is.na(n_animals_w_detections), 0, n_animals_w_detections)) %>%
   mutate_at(.vars = c("site_latitude", "site_longitude"), ~as.numeric(.)) %>%
@@ -139,12 +139,12 @@ left_join(viral_testing_by_species_and_site, viral_detections_by_species_and_sit
 species.to.exclude <- c("Chiroptera", "Rhinopomatidae", "Rodentia")
 
 p2.associations <- d2 %>%
-  distinct(country, viral_species, class, species_scientific_name) %>%
+  distinct(country, viral_species, class, scientific_name) %>%
   filter(
     !is.na(viral_species),
     class == "Mammalia",
-    !str_detect(species_scientific_name, " sp."),
-    !(species_scientific_name %in% species.to.exclude)
+    !str_detect(scientific_name, " sp."),
+    !(scientific_name %in% species.to.exclude)
   ) %>%
   select(-class) %>%
   write_csv(., h("data", "P2_virus_mammal_host_associations.csv"))
